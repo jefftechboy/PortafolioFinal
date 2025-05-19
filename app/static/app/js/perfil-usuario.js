238fd9a9c638b7c2d07087d6e0871dc474190946
Sebastian Mejias Mateluna <sebastianmajias@gmail.com>
2025-05-19 17:18:19 -0400

    Conecciones de paginas seba

---
+++ app/static/app/js/perfil-usuario.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const logoImg = document.querySelector('.logo');
+  if (logoImg) {
+    logoImg.style.cursor = 'pointer';
+    logoImg.addEventListener('click', function () {
+      window.location.href = '/inicio.html/';
+    });
+  }
+});
+document.addEventListener('DOMContentLoaded', function () {
+  // Redirigir al login desde el ícono de perfil
+  const perfilIcon = document.querySelector('.perfil-icon');
+  if (perfilIcon) {
+    perfilIcon.addEventListener('click', function () {
+      window.location.href = '/login/';
+    });
+  }
+
+  // Enlaces del navbar
+  const navLinks = document.querySelectorAll('nav a');
+  if (navLinks.length) {
+    navLinks[0].addEventListener('click', () => window.location.href = '/agenda/');
+    navLinks[1].addEventListener('click', () => window.location.href = '/servicios/');
+    navLinks[2].addEventListener('click', () => window.location.href = '/habitaciones/');
+    navLinks[3].addEventListener('click', () => window.location.href = '/reserva/'); // ya está activo
+    navLinks[4].addEventListener('click', () => window.location.href = '/agenda/');
+  }
+});
---
+++ app/static/app/js/servicio-emp.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const logoImg = document.querySelector('.logo');
+  if (logoImg) {
+    logoImg.style.cursor = 'pointer';
+    logoImg.addEventListener('click', function () {
+      window.location.href = '/inicio.html/';
+    });
+  }
+});
+document.addEventListener('DOMContentLoaded', function () {
+  // Redirigir al login desde el ícono de perfil
+  const perfilIcon = document.querySelector('.perfil-icon');
+  if (perfilIcon) {
+    perfilIcon.addEventListener('click', function () {
+      window.location.href = '/login/';
+    });
+  }
+
+  // Enlaces del navbar
+  const navLinks = document.querySelectorAll('nav a');
+  if (navLinks.length) {
+    navLinks[0].addEventListener('click', () => window.location.href = '/habitaciones/');
+    navLinks[1].addEventListener('click', () => window.location.href = '/cliente-emp/');
+    navLinks[2].addEventListener('click', () => window.location.href = '/reserva/'); // ya está activo
+    
+  }
+});
